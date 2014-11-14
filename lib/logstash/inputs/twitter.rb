7c8f6ce55a4592cf2ad34bded1e05386982ed631
Pier-Hugues Pellerin <phpellerin@gmail.com>
2014-11-14 13:24:47 -0500

    Adding guard for the twitter gem monkeypatch and raising a more meaningful error
    Fixes #4

--- lib/logstash/inputs/twitter.rb
+++ lib/logstash/inputs/twitter.rb
@@ -56,6 +56,11 @@ class LogStash::Inputs::Twitter < LogStash::Inputs::Base
   def register
     require "twitter"
 
+    # monkey patch twitter gem to ignore json parsing error.
+    # at the same time, use our own json parser
+    # this has been tested with a specific gem version, raise if not the same
+    raise("Incompatible Twitter gem version and the LogStash::Json.load") unless Twitter::Version.to_s == "5.12.0"
+
     Twitter::Streaming::Response.module_eval do
       def on_body(data)
         @tokenizer.extract(data).each do |line|
