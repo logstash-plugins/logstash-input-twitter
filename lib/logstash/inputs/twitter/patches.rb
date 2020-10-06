237d8735ac8a87900c670ab8a3e407baa44f2561
Karol Bucek <self@kares.org>
2020-10-06 10:59:33 +0200

    Chore: additional version checks and notes

--- lib/logstash/inputs/twitter/patches.rb
+++ lib/logstash/inputs/twitter/patches.rb
@@ -15,10 +15,16 @@ module LogStash
 
       def self.verify_version
         raise("Incompatible Twitter gem version and the LogStash::Json.load") unless ::Twitter::Version.to_s == "6.2.0"
+        # NOTE: we're also known to work with twitter gem version 7.0 which uses http 4.x
+        raise("Incompatible HTTP gem version: #{HTTP::VERSION}") if HTTP::VERSION >= '5.0' || HTTP::VERSION < '3.0'
+        # after a major upgrade, unless CI is running integration specs, please run integration specs manually
       end
 
       def self.patch_json
         ::Twitter::Streaming::Response.module_eval do
+          unless method_defined? :on_body
+            raise "::Twitter::Streaming::Response#on_body not defined!" # patch bellow needs a review
+          end
           def on_body(data)
             @tokenizer.extract(data).each do |line|
               next if line.empty?
@@ -33,7 +39,13 @@ module LogStash
       end
 
       def self.patch_http_request
+        # NOTE: we might decide to drop the patch at some point, for now proxies such as squid
+        # are opinionated about having full URIs in the http head-line (having only /path fails
+        # with 400 Bad Request in Squid 4.10).
         ::HTTP::Request.class_eval do
+          unless method_defined? :headline
+            raise "::HTTP::Request#headline not defined!" # patch bellow needs a review
+          end
           def headline
             request_uri =
                 if using_proxy? #&& !uri.https?
